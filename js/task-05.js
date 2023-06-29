a0282176c59e9bda4f1e7425066448e6e4df67e6
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2023-06-29 21:38:40 +0200

    changes

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,19 @@
+// Завдання 5
+
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+//   <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
 const formEl = document.querySelector("#name-input");
 const nameEl = document.querySelector("#name-output");
 
 formEl.addEventListener("input", addName);
 
 function addName(event) {
-  nameEl.textContent = formEl.value;
+  if (formEl.value === "") {
+    nameEl.textContent = "Anonymous";
+    return;
+  }
+  nameEl.textContent = formEl.value.trim();
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
+// Завдання 10 (виконувати не обов'язково)
+
+// Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
+
+// <div id="controls">
+//   <input type="number" min="1" max="100" step="1" />
+//   <button type="button" data-create>Create</button>
+//   <button type="button" data-destroy>Destroy</button>
+// </div>
+
+// <div id="boxes"></div>
+
+// Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+
 const inputEl = document.querySelector("#controls");
 const boxesEl = document.querySelector("#boxes");
 // const btnCreate = document.querySelector("button[data-create]");
@@ -17,12 +42,18 @@ function getRandomHexColor() {
 function createBoxes(amount) {
   amount = inputEl.children[0].value;
   const firstSize = 50;
+  let boxesArr = [];
+
   for (let i = 0; i < amount; i += 1) {
     const divBox = document.createElement("div");
     const size = firstSize + i * 10;
     divBox.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}; margin-bottom: 10px`;
-    boxesEl.append(divBox);
+    boxesArr.push(divBox);
+
+    // boxesEl.append(divBox);
   }
+
+  boxesEl.append(...boxesArr);
 }
 
 function destroyBoxes(event) {
